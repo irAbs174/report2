9cca620923e6e53cd5425d104598422e2ecc8f00
irAbs174 <damerchiloa@gmail.com>
2024-07-19 06:48:57 +0000

    upload frontend

---
+++ frontend/src/app/add-code/add-code.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-code',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-code.component.html',
+  styleUrl: './add-code.component.css'
+})
+export class AddCodeComponent {
+
+}

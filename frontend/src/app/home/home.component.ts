e3c4000f474ff80570f2d78101f9483e7d72a65a
irAbs174 <damerchiloa@gmail.com>
2024-07-21 17:38:26 +0000

    stable v => with 3 component @auth @verfyCodeFront @home

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -2,20 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet, Router } from '@angular/router';
 import { NgModule } from '@angular/core';
 
+const countdown = (seconds: number) => {
+  let remainingTime = seconds;
+
+  const interval = setInterval(() => {
+      console.log(remainingTime);
+      remainingTime--;
+
+      if (remainingTime < 0) {
+          clearInterval(interval);
+          console.log('hello');
+      }
+  }, 1000);
+};
+
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
-  styleUrls: ['../app.component.css']
+  styleUrl: './home.component.css',
 })
-export class HomeComponent implements OnInit {
-
-  constructor(
-    private router: Router
-  ) { }
-
-  ngOnInit() {
-
-  }
+export class HomeComponent {
 
 }
+
+
+

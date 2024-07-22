aaf8873d86ecc09c6e970a9065f4daffe08b3566
irAbs174 <Arashsorosh174@gmail.com>
2024-07-22 09:39:21 +0000

    venv

--- app/frontend/src/app/report/report.component.ts
+++ app/frontend/src/app/report/report.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import {LoadingService} from '../loading.service';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+
 
 @Component({
   selector: 'app-report',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, MatGridListModule],
   templateUrl: './report.component.html',
   styleUrl: './report.component.css'
 })
